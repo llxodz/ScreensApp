a7dc77b9558fc0ab9134d7776eda64ce5ec76600
Ilya Gavrilov <llxodz@gmail.com>
2022-01-30 15:28:08 +0300

    Сreated the clickability of the element

---
+++ app/src/main/java/ru/llxodz/screensapp/PaymentModel.kt
@@ -0,0 +1,7 @@
+package ru.llxodz.screensapp
+
+data class PaymentModel(
+    val subscriptionTime: String,
+    val subscriptionPrice: Int,
+    val subscriptionDescription: String
+)
--- app/src/main/java/ru/llxodz/screensapp/PaymentProcessFragment.kt
+++ app/src/main/java/ru/llxodz/screensapp/PaymentProcessFragment.kt
@@ -1,24 +1,30 @@
 package ru.llxodz.screensapp
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.LinearLayoutManager
 import ru.llxodz.screensapp.databinding.FragmentPaymentProcessBinding
 
-class PaymentProcessFragment : Fragment() {
+class PaymentProcessFragment : Fragment(), PaymentMethodsListAdapter.OnItemClickListener {
 
     private var _binding: FragmentPaymentProcessBinding? = null
     private val binding get() = _binding!!
     private lateinit var recyclerAdapter: PaymentMethodsListAdapter
+    private val exampleList = arrayListOf<PaymentModel>()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
         _binding = FragmentPaymentProcessBinding.inflate(inflater, container, false)
+        exampleList.add(PaymentModel("1 месяц", 599, "Ежемесячная подписка"))
+        exampleList.add(PaymentModel("2 месяц", 599, "Ежемесячная подписка"))
+        exampleList.add(PaymentModel("3 месяца", 59922, "Ежемесячная подписка"))
+//        exampleList.add(PaymentModel("4 месяца", 599, "Ежемесячная подписка"))
+//        exampleList.add(PaymentModel("12 месяцев", 1012, "Ежемесячная подписка"))
         return binding.root
     }
 
@@ -36,7 +42,7 @@ class PaymentProcessFragment : Fragment() {
     private fun showPaymentMethodsAdapter() {
         val recyclerView = binding.rvMethodsPayment
         recyclerView.layoutManager = LinearLayoutManager(context)
-        recyclerAdapter = PaymentMethodsListAdapter()
+        recyclerAdapter = PaymentMethodsListAdapter(requireActivity(), exampleList, this)
         recyclerView.adapter = recyclerAdapter
     }
 
@@ -44,5 +50,9 @@ class PaymentProcessFragment : Fragment() {
         @JvmStatic
         fun newInstance() =
             PaymentProcessFragment()
+    }
+
+    override fun onItemClick(position: Int) {
+        recyclerAdapter.notifyItemChanged(position)
     }
 }
